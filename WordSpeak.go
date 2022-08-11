a0f66c57121bf8fe283f99e20be5a97aeb4ebe1c
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-11 23:24:33 +0200

    do-end semantics, from pang-001

--- WordSpeak.go
+++ WordSpeak.go
@@ -10,7 +10,7 @@ func main() {
 		"print": 1,
 		"add":   2,
 	}
-	pn := "print 1 print add 1 2 print 4"
+	pn := "print 1 do print add 1 2 print 4 end"
 	words := strings.Split(pn, " ")
 	wordIndex := 2
 	fmt.Println(phraseLength(words, wordIndex, definitions))
@@ -19,13 +19,26 @@ func main() {
 func phraseLength(words []string, wordIndex int, definitions map[string]int) int {
 	word := words[wordIndex]
 	length := 1
+
 	_, err := strconv.ParseInt(word, 10, 64)
 	if err == nil {
-		return 1
+		return length
 	}
+
+	if word == "do" {
+		for {
+			if words[wordIndex+length] == "end" {
+				length += 1
+				return length
+			}
+			length += phraseLength(words, wordIndex+length, definitions)
+		}
+	}
+
 	argumentLength := definitions[word]
 	for argumentIndex := 1; argumentIndex <= argumentLength; argumentIndex++ {
 		length += phraseLength(words, wordIndex+length, definitions)
 	}
+
 	return length
 }
